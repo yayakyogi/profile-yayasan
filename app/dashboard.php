e7ecaa8f27d7cc2700a5bedc97e026315b2a9b06
Yayak Yogi <yayaktaka@gmail.com>
2021-12-02 05:36:19 +0700

    remove breadcrumb

--- app/dashboard.php
+++ app/dashboard.php
@@ -2,12 +2,7 @@
   function dashboard()
   {
 ?>
-  <h1 class="mt-4">Dashboard</h1>
-  
-  <ol class="breadcrumb mb-4">
-    <li class="breadcrumb-item active">Beranda</li>
-  </ol>
-    
+  <h1 class="mt-4 mb-3">Dashboard</h1>
   <div class="row">
     <div class="col-xl-3 col-md-6">
       <div class="card bg-primary text-white mb-4">
