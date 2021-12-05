818f47c26fdda98008503c6c3766b723cd3aa5e0
Yayak Yogi <yayaktaka@gmail.com>
2021-12-05 19:36:10 +0700

    fix update data information

--- app/information.php
+++ app/information.php
@@ -114,7 +114,7 @@
 
               <div class="card-footer bg-white pt-0 pb-3">
                 <a href="?pages='.$pages.'&views=add" class="btn btn-sm btn-light px-3 mt-3"><i class="fas fa-plus"></i> Tambah</a>
-                <a href="?pages='.$pages.'&views=edit" class="btn btn-sm btn-primary px-3 mt-3"><i class="fas fa-edit"></i> Edit</a>
+                <a href="?pages='.$pages.'&views=edit&id='.$data['id'].'" class="btn btn-sm btn-primary px-3 mt-3"><i class="fas fa-edit"></i> Edit</a>
               </div>
           </div><!-- ./card -->
         ';
@@ -257,6 +257,29 @@
 
       if($views === 'edit')
       {
+        $id = GET('id','');
+        if($exec && $id)
+        {
+          $query_update = "UPDATE tb_information SET 
+                          name='$name',email='$email',phone='$phone',address='$address',
+                          facebook_name='$facebook_name',facebook_link='$facebook_link',
+                          instagram_name='$instagram_name',instagram_link='$instagrem_link',
+                          youtube_name='$youtube_name',youtube_link='$youtube_link',
+                          updated_at=NOW()
+                          WHERE id='$id'
+                          ";
+          $sql_update = mysqli_query($conn,$query_update);
+          if($sql_update)
+          {
+            GET('exec','');
+            echo '<script>window.location="?pages='.$pages.'&views=index"</script>';
+          }
+        }
+
+        $query_edit = "SELECT * FROM tb_information WHERE id='$id'";
+        $sql_edit = mysqli_query($conn,$query_edit);
+        $data = mysqli_fetch_assoc($sql_edit);
+
         echo '
             <div class="card mt-4">
               <div class="card-header bg-white">
@@ -264,11 +287,13 @@
               </div>
               <div class="card-body pe-5">
                 <form method="POST" action="?pages='.$pages.'&views='.$views.'">
+                  <input type="hidden" name="exec" value="'.time().'"/>
+                  <input type="hidden" name="id" value="'.$data['id'].'"/>
                   <h5 class="fw-normal">Profil</h5>
                   <div class="row mb-2">
                       <label for="name" class="col-sm-2 col-form-label">Nama Yayasan</label>
                       <div class="col-sm-10">
-                        <input type="text" class="form-control" name="name" id="name" placeholder="Masukkan nama yayasan">
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Masukkan nama yayasan" value="'.$data['name'].'">
                       </div>
                   </div><!-- ./name -->
 
@@ -277,7 +302,7 @@
                       <div class="row mb-2">
                         <label for="telephone" class="col-sm-4 col-form-label">Telepon</label>
                         <div class="col-sm-8">
-                          <input type="text" class="form-control" name="phone" id="telephone" placeholder="Masukkan nomor telepon">
+                          <input type="text" class="form-control" name="phone" id="telephone" placeholder="Masukkan nomor telepon" value="'.$data['phone'].'">
                         </div>
                       </div>
                     </div><!-- ./phone -->
@@ -286,7 +311,7 @@
                       <div class="row mb-2">
                         <label for="email" class="col-sm-3 col-form-label">Email</label>
                         <div class="col-sm-9">
-                          <input type="email" class="form-control" id="email" placeholder="Masukkan email">
+                          <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan email" value="'.$data['email'].'">
                         </div>
                       </div>
                     </div><!-- ./email -->
@@ -295,7 +320,7 @@
                   <div class="row mb-2">
                     <label for="address" class="col-sm-2 col-form-label">Alamat</label>
                     <div class="col-sm-10">
-                      <textarea class="form-control" name="address id="address" placeholder="Masukkan alamat lengkap"></textarea>
+                      <textarea class="form-control" name="address" id="address" placeholder="Masukkan alamat lengkap">'.$data['address'].'</textarea>
                     </div>
                   </div><!-- ./alamat -->
 
@@ -306,16 +331,16 @@
                       <div class="row mb-2">
                         <label for="facebook_name" class="col-sm-4 col-form-label">Nama Facebook</label>
                         <div class="col-sm-8">
-                          <input type="text" class="form-control" name="facebook_name" id="facebook_name" placeholder="Masukkan nama facebook">
+                          <input type="text" class="form-control" name="facebook_name" id="facebook_name" placeholder="Masukkan nama facebook" value="'.$data['facebook_name'].'">
                         </div>
                       </div>
                     </div><!-- ./name profile facebook -->
 
                     <div class="col-12 col-sm-6">    
                       <div class="row mb-2">
-                        <label for="facebook_name" class="col-sm-2 col-form-label">Link</label>
+                        <label for="facebook_link" class="col-sm-2 col-form-label">Link</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="facebook_name" placeholder="Link facebook yayasan">
+                          <input type="text" class="form-control" name="facebook_link" id="facebook_link" placeholder="Link facebook yayasan" value="'.$data['facebook_link'].'">
                         </div>
                       </div>
                     </div><!-- ./link -->
@@ -327,16 +352,16 @@
                       <div class="row mb-2">
                         <label for="instagram_name" class="col-sm-4 col-form-label">Nama Instagram</label>
                         <div class="col-sm-8">
-                          <input type="text" class="form-control" name="instagram_name" id="instagram_name" placeholder="Masukkan nama instagram">
+                          <input type="text" class="form-control" name="instagram_name" id="instagram_name" placeholder="Masukkan nama instagram" value="'.$data['instagram_name'].'">
                         </div>
                       </div>
                     </div><!-- ./name profile instagram -->
 
                     <div class="col-12 col-sm-6">    
                       <div class="row mb-2">
-                        <label for="instagram_name" class="col-sm-2 col-form-label">Link</label>
+                        <label for="instagram_link" class="col-sm-2 col-form-label">Link</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="instagram_name" placeholder="Link instagram yayasan">
+                          <input type="text" class="form-control" name="instagram_link" id="instagram_link" placeholder="Link instagram yayasan" value="'.$data['instagram_link'].'">
                         </div>
                       </div>
                     </div><!-- ./link -->
@@ -348,16 +373,16 @@
                       <div class="row mb-2">
                         <label for="youtube_name" class="col-sm-4 col-form-label">Nama YoutTube</label>
                         <div class="col-sm-8">
-                          <input type="text" class="form-control" name="youtube_name" id="youtube_name" placeholder="Masukkan nama youtube">
+                          <input type="text" class="form-control" name="youtube_name" id="youtube_name" placeholder="Masukkan nama youtube" value="'.$data['youtube_name'].'">
                         </div>
                       </div>
                     </div><!-- ./name profile youtube -->
 
                     <div class="col-12 col-sm-6">    
                       <div class="row mb-2">
-                        <label for="youtube_name" class="col-sm-2 col-form-label">Link</label>
+                        <label for="youtube_link" class="col-sm-2 col-form-label">Link</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="youtube_name" placeholder="Link youtube yayasan">
+                          <input type="text" class="form-control" name="youtube_link" id="youtube_link" placeholder="Link youtube yayasan" value="'.$data['youtube_link'].'">
                         </div>
                       </div>
                     </div><!-- ./link -->
