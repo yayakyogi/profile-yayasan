003653726b74ef52925d8e93e476bef9b65a4da8
Yayak Yogi <yayaktaka@gmail.com>
2021-12-12 09:39:29 +0700

    remove padding

--- detail.php
+++ detail.php
@@ -40,18 +40,16 @@
         <div class="col-sm-9 pe-2">
 
           <!-- content-detail -->
-          <div class="px-2">
-            <h4 class="mb-4" data-aos="fade-up">Detail Postingan</h2>
-            <img src="./public/img_cover/<?php echo $data['img_cover'] ?>" class="img-fluid rounded" data-aos="fade-up" data-aos-duration="1200"/>
-            <h1 class="mt-3 fs-2" data-aos="fade-up" data-aos-duration="1300"><?php echo $data['title'] ?></h1>
-            <p class="upload px-2" data-aos="fade-up" data-aos-duration="1400">
-              <span><i class="fas fa-calendar-alt"></i> <?php echo date("d / m / Y", strtotime($data['created_at']))?></span>
-              <span> <i class="fas fa-user-edit"></i> <?php echo $data['author']?></span>
-            </p>
-            <div data-aos="fade-up" data-aos-duration="1200">
-              <?php echo html_entity_decode($data['content'])?>
-            </div>
-          </div><!-- ./p-2 -->
+          <h4 class="mb-4" data-aos="fade-up">Detail Postingan</h2>
+          <img src="./public/img_cover/<?php echo $data['img_cover'] ?>" class="img-fluid rounded" data-aos="fade-up" data-aos-duration="1200"/>
+          <h1 class="mt-3 fs-2" data-aos="fade-up" data-aos-duration="1300"><?php echo $data['title'] ?></h1>
+          <p class="upload px-2" data-aos="fade-up" data-aos-duration="1400">
+            <span><i class="fas fa-calendar-alt"></i> <?php echo date("d / m / Y", strtotime($data['created_at']))?></span>
+            <span> <i class="fas fa-user-edit"></i> <?php echo $data['author']?></span>
+          </p>
+          <div data-aos="fade-up" data-aos-duration="1200">
+            <?php echo html_entity_decode($data['content'])?>
+          </div>
           <!-- ./content-detail -->
           
           </br>
@@ -61,7 +59,7 @@
             <h5 class="me-1 category w-25">Postigan Serupa</h5>
             <hr style="border-color: #545961; background-color: #545961; opacity: 0.2; margin: 0 !important;"/>
           </div>
-          <div class="row g-3 px-2">
+          <div class="row g-3">
             <?php while($data = mysqli_fetch_assoc($sql_type)){ ?>
               <div class="col-sm-3 mb-3">
                 <div class="card shadow-sm rounded overflow-hidden" data-aos="fade-up" data-aos-duration="1800">
