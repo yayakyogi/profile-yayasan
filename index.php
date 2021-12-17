9a9dd7d9d48aaae3503d6c86b7a4adbaf10c570c
yayakyogi <yayaktaka@gmail.com>
2021-12-17 21:52:16 +0700

    fix layouting

--- index.php
+++ index.php
@@ -26,17 +26,6 @@
   $query_type_article = "SELECT * FROM tb_post WHERE type='Artikel'";
   $sql_type_articel = mysqli_query($conn,$query_type_news);
 
-  // get data post announcement
-  $query_type_announcement = "SELECT * FROM tb_post WHERE type='Pengumuman'";
-  $sql_type_announcement = mysqli_query($conn,$query_type_announcement);
-
-  // get data from tb_type
-  $query_type = "SELECT * FROM tb_type";
-  $sql_type = mysqli_query($conn,$query_type);
-
-  // get data from tb_category
-  $query_category = "SELECT * FROM tb_category";
-  $sql_category = mysqli_query($conn,$query_category);
 
   // pagination
   $limit = 7;
@@ -76,7 +65,7 @@
           {
             $i++;
             echo '
-              <div class="'.($i === 1 ? 'carousel-item active' : 'carousel-item ').'"  data-bs-interval="5000">
+              <div class="'.($i === 1 ? 'carousel-item active' : 'carousel-item ').'" data-bs-interval="2000">
                 <img src="./public/img_cover/'.$data['img_cover'].'" class="w-100 h-100" style="object-fit: cover;object-position: center;" alt="...">
                 <div class="carousel-caption d-block text-start" data-aos="fade-up" data-aos-duration="1800">
                   <a href="detail.php?id='.$data['id'].'">
@@ -139,7 +128,7 @@
           </div><!-- ./row -->
        </div><!-- ./col-sm-7 col-1 -->
        <!-- ./news post -->
-       
+
        <!-- foto -->
        <div class="col-sm-5" data-aos="fade-up" data-aos-duration="1600">
           <div class="row g-1">
@@ -163,10 +152,105 @@
   </section><!-- ./section -->
   <!-- ./section post and foto -->
 
-<?php footer() ?>
+  <!-- section all post -->
+  <section class="news">
+    <div class="content">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="d-flex justify-content-start align-items-center my-3" data-aos="fade-up" data-aos-duration="1500">
+            <h5 class="me-1 category w-25" data-aos="fade-up" data-aos-duration="1000">Semua Postingan</h5>
+            <hr style="border-color: #545961; width:75%; background-color: #545961; opacity: 0.2; margin: 0 !important;" data-aos="fade-up" data-aos-duration="1100"/>
+          </div>
+          <div class="row g-3">
+            <?php while($data = mysqli_fetch_assoc($sql)){ $i=1; ?>
+              <div class="col-sm-4 mb-3">
+                <div class="card shadow-sm rounded overflow-hidden" data-aos="fade-up" data-aos-duration="1200">
+                  <div class="card-body p-0">
+                    <div class="w-100">
+                      <img src="./public/img_cover/<?php echo $data['img_cover'] ?>" class="w-100" style="height:120px"/>
+                    </div>
+                    <div class="p-2">
+                      <p class="datetime">
+                        <span><i class="fas fa-calendar-alt"></i> <?php echo date("d / m / Y", strtotime($data['created_at']))?></span>
+                        <span> <i class="fas fa-user-edit"></i> <?php echo $data['author']?></span>
+                      </p>
+                      <h6>
+                          <a href="detail.php?id=<?php echo $data['id'] ?>">
+                            <?php echo $data['title']?>
+                          </a>
+                      </h6>
+                      <small class="text-secondary" style="font-size:0.7rem"><i><?php echo '#'.$data['category'].'  #'.$data['type'];?></i></small>
+                    </div><!-- ./p-2 -->
+                  </div><!-- ./card-body -->
+                </div><!-- ./card -->
+              </div><!-- ./col-sm-3 -->
+            <?php 
+            } 
+            if(mysqli_num_rows($sql) <= 0) echo '<p class="lead">Data Kosong</p>'
+            ?>
+            <?php 
+              echo '
+              <!-- Pagination -->
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">';
+                  // page previous
+                  if($page > 1)  
+                    echo '<a class="page-link" href="?page='.$prev.'"><span aria-hidden="true">&laquo;</span></a>';
+                  else 
+                    echo '<li class="page-item disabled"><span class="page-link">&laquo;</span></li>';
+                  echo '</li>';
+                  // show list page
+                  for ($i=1; $i <= $total_page; $i++) { 
+                    if($page == $i)
+                      echo '<li class="page-item active" aria-current="page"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
+                    else 
+                      echo '<li class="page-item"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
+                  }
+                  // page next
+                  echo '
+                  <li class="page-item">';
+                    if($page < $count) 
+                      echo '<a class="page-link" href="?page='.$next.'"><span aria-hidden="true">&raquo;</span></a>';
+                    else echo '<li class="page-item disabled"><span class="page-link">&raquo;</span></li>';
+                  echo '
+                  </li>
+                </ul><!-- ./pagination -->
+              </nav><!-- ./aria label pagination -->
+              ';
+            ?>
+          </div><!-- ./row -->
+        </div><!-- ./col-sm-8 -->
 
+        <div class="col-sm-4">
+          <div class="d-flex justify-content-start align-items-center mt-3 mb-2" data-aos="fade-up" data-aos-duration="1500">
+            <h5 class="me-1 category w-25" data-aos="fade-up" data-aos-duration="1000">Linimasa</h5>
+            <hr style="border-color: #545961; width:75%; background-color: #545961; opacity: 0.2; margin: 0 !important;" data-aos="fade-up" data-aos-duration="1100"/>
+          </div>
+          <h6 data-aos="fade-up" data-aos-duration="1750">Facebook</h6>
+          <div class="container-iframe" data-aos="fade-up" data-aos-duration="1750">
+            <iframe 
+              class="responsive-iframe"
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" 
+              scrolling="no" 
+              frameborder="0" 
+              allowfullscreen="true" 
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+            </iframe>
+          </div>
+          <h6 class="my-3" data-aos="fade-up" data-aos-duration="1750">YouTube</h6>
+          <div class="container-iframe mb-5" data-aos="fade-up" data-aos-duration="2000">
+            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/8fho3489C8A"></iframe>
+          </div>
+        </div><!-- ./col-sm-4 -->
+      </div><!-- ./row -->
+    </div><!-- ./content -->
+  </section><!-- ./section -->
+  <!-- ./section all post -->
+
+<?php footer() ?>
 </body>
-<script src="./public/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="./public/bootstrap-5.0.2/dist/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
   AOS.init();
